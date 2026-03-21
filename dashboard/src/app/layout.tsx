f6e5747f9aad2fbb05819fec878f78813c638766
luliquintino <luliquintino@users.noreply.github.com>
2026-03-21 09:32:06 -0300

    fix: correct manifest background_color, add scope+lang, fix status bar style and splash orientation

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -27,19 +27,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         {/* iOS PWA */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Clarita" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
         {/* iOS Splash Screens */}
         <link rel="apple-touch-startup-image" href="/splash/splash-2796x1290.png"
-          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)" />
+          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="/splash/splash-2556x1179.png"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)" />
+          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="/splash/splash-2532x1170.png"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)" />
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="/splash/splash-1334x750.png"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
       </head>
       <body className={`${inter.className} antialiased`}>
         <ServiceWorkerRegistration />
